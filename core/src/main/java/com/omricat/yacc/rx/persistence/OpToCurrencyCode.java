32e6f9e3ed8e745d1ccfd2f1b6b00d6c09cd01e3
Joseph Cooper <cooper.joseph@gmail.com>
2015-01-29 17:41:28 +0100

    Added OpToCurrencyCode class to convert apply the operation represented by an instance of Operation<CurrencyCode> to an instance of RxSet<CurrencyCode>

---
+++ core/src/main/java/com/omricat/yacc/rx/persistence/OpToCurrencyCode.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2015 Omricat Software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.omricat.yacc.rx.persistence;
+
+import com.omricat.yacc.model.CurrencyCode;
+import com.omricat.yacc.rx.RxSet;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+import rx.Observable;
+import rx.functions.Func1;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class OpToCurrencyCode implements
+        Func1<Operation<CurrencyCode>, Observable<? extends
+                Set<CurrencyCode>>> {
+
+    private final RxSet<CurrencyCode> rxSet;
+
+    public OpToCurrencyCode(@NotNull final RxSet<CurrencyCode> rxSet) {
+        this.rxSet = checkNotNull(rxSet);
+    }
+
+    @NotNull
+    @Override
+    public Observable<? extends Set<CurrencyCode>> call(final
+                                                        Operation<CurrencyCode> op) {
+        return new OpVisitor().call(op);
+    }
+
+    private final class OpVisitor implements Operation
+            .Visitor<CurrencyCode> {
+
+        private Observable<? extends Set<CurrencyCode>> returnObservable;
+
+        private Observable<? extends Set<CurrencyCode>> call
+                (Operation<CurrencyCode> op) {
+            op.accept(this);
+            return returnObservable;
+        }
+
+        @Override
+        public void visit(final Operation.Add<CurrencyCode> addOp) {
+            returnObservable = rxSet.add(addOp.value);
+        }
+
+        @Override
+        public void visit(final Operation.Remove<CurrencyCode> removeOp) {
+            returnObservable = rxSet.remove(removeOp.value);
+        }
+
+
+    }
+}
